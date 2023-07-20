cbeee21da5fc57b8ce73a74306f24edc17857d3d
a.iskhakov <a.iskhakov@vk.team>
2023-07-20 23:07:44 +0300

    add sql file

---
+++ ddl/data_portal_ch_migration/ch_3.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS test_table
+(
+    app_id         Int64,
+    name           String,
+    device_os      String,
+    dt             Date
+)
+    ENGINE = MergeTree
+        PARTITION BY toYYYYMMDD(dt)
+        ORDER BY name;
